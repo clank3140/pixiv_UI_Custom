d288b86f7986153711a96a9bd7d4531469bd5671
ryo <req0920fse@gmail.com>
2020-02-24 14:08:45 +0900

    右上にまとめられてしまったメニューを再びバー状に

---
+++ content_script_pixiv.js
@@ -0,0 +1,9 @@
+$(function () {
+    $("#wrapper").prepend("<a class=\"addedMenu\" href =\"https://www.pixiv.net/setting_user.php\">設定</a>");
+    $("#wrapper").prepend("<a class=\"addedMenu\" href =\"https://www.pixiv.net/novel/marker_all.php\">しおり</a>");
+    $("#wrapper").prepend("<a class=\"addedMenu\" href =\"https://www.pixiv.net/history.php\">閲覧履歴</a>");
+    $("#wrapper").prepend("<a class=\"addedMenu\" href =\"https://www.pixiv.net/bookmark.php\">ブックマーク</a>");
+    $("#wrapper").prepend("<a class=\"addedMenu\" href =\"https://www.pixiv.net/users/5133273/followers/\">フォロワー</a>");
+    $("#wrapper").prepend("<a class=\"addedMenu\" href =\"https://www.pixiv.net/users/5133273/following/\">フォロー</a>");
+    $("#wrapper").prepend("<a class=\"addedMenu\" href =\"https://www.pixiv.net/manage/illusts/\">作品管理</a>");
+});
